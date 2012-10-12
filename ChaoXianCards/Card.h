4ff9b815d81eb0c1ac51ac1e6cdaa85f47669690
Kelv <kelv@138-38-184-64.eduroam.bath.ac.uk>
2012-10-12 15:20:42 +0100

    Saving all my changes

---
+++ ChaoXianCards/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  ChaoXianCards
+//
+//  Created by Kelv on 16/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ ChaoXianCards/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  ChaoXianCards
+//
+//  Created by Kelv on 16/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
